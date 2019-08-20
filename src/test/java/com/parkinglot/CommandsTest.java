1df1c5d3d01a8e2db36fd6bf69e30d5dfe47a574
Pranav <pranav.chandra08@gmail.com>
2019-08-20 08:49:13 +0530

    Adding the test case of the problem.

---
+++ src/test/java/com/parkinglot/CommandsTest.java
@@ -0,0 +1,16 @@
+package com.parkinglot;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommandsTest {
+    Commands commands = new Commands();
+    @Test
+    public void checkCommandInList() throws Exception {
+        assertFalse(commands.commandsMap.isEmpty());
+        assertTrue(commands.commandsMap.containsKey("create_parking_lot"));
+        assertFalse(commands.commandsMap.containsKey("mytestcommand"));
+    }
+}
